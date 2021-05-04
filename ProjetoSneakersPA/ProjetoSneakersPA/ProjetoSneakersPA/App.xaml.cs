c8a005c926076639170952ab0481eb4c38e760c9
adrianodlucca <adriano@azuris.com.br>
2021-05-04 01:30:17 -0300

    implemtacao do titulo e searchbar

--- ProjetoSneakersPA/ProjetoSneakersPA/ProjetoSneakersPA/App.xaml.cs
+++ ProjetoSneakersPA/ProjetoSneakersPA/ProjetoSneakersPA/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjetoSneakersPA.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace ProjetoSneakersPA
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new SneakersPage();
         }
 
         protected override void OnStart()
