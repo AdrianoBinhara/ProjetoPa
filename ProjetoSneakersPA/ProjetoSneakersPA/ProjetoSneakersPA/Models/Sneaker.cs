991bfcdd4d41b32759f553f9e68cb5dd1965ac67
adrianodlucca <adriano@azuris.com.br>
2021-05-04 01:49:42 -0300

    criação da viewmodel e lista de sneakers

---
+++ ProjetoSneakersPA/ProjetoSneakersPA/ProjetoSneakersPA/Models/Sneaker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoSneakersPA.Models
+{
+   public class Sneaker
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Price { get; set; }
+        public string Picture { get; set; }
+    }
+}
---
+++ ProjetoSneakersPA/ProjetoSneakersPA/ProjetoSneakersPA/ViewModel/SneakersViewModel.cs
@@ -0,0 +1,25 @@
+﻿using ProjetoSneakersPA.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ProjetoSneakersPA.ViewModel
+{
+    public class SneakersViewModel
+    {
+
+        public ObservableCollection<Sneaker> SneakersList { get; set; }
+        public SneakersViewModel()
+        {
+            SneakersList = new ObservableCollection<Sneaker>
+            {
+                new Sneaker{ Name="React Scape", Model="Woman running shoes", Price="135"},
+                new Sneaker{ Name="Zoom Elx", Model="Woman running shoes", Price="143"},
+                new Sneaker{ Name="Jumpo all", Model="Woman running shoes", Price="195"},
+                new Sneaker{ Name="Trace Run", Model="Woman running shoes", Price="175"},
+                new Sneaker{ Name="Supazi Scale", Model="Woman running shoes", Price="105"}
+            };
+        }
+    }
+}
